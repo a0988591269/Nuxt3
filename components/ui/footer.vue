f187ef4d8b9e26350909c7ebafc36fe360b20098
Benson <a0988591269@gmail.com>
2025-08-04 22:21:48 +0800

    commit

--- components/ui/footer.vue
+++ components/ui/footer.vue
@@ -2,10 +2,10 @@
   <footer>
     <div class="mx-auto flex h-24 items-center justify-between px-6">
       <NuxtLink to="/" class="logo-font">
-        conduit
+        Nuxt3
       </NuxtLink>
       <span>
-        An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code & design licensed under MIT.
+        An learning project from <a href="https://github.com/a0988591269/Nuxt3">Benson Pang</a>. Code & design.
       </span>
     </div>
   </footer>
