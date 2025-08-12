19b42bc4d91e587f20f18b9dc79084e1f8ed632e
MSI\a0988 <a0988591269@gmail.com>
2025-08-12 17:11:40 +0800

    20250812-add Data Fetching

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,24 @@
         <Icon class="h-12 w-12 text-yellow-400" name="heroicons:sun" />
         <FirstComponent />
         <div class="my-4 flex space-x-4">
-          <ClientOnly>
-            <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
-              <NuxtLink to="/about">前往 About</NuxtLink>
-            </BaseButton>
-            <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
-              <NuxtLink to="/contact">前往 Contact</NuxtLink>
-            </BaseButton>
-            <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
-              <NuxtLink to="/counter">前往 Counter</NuxtLink>
-            </BaseButton>
-          </ClientOnly>
+          <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
+            <NuxtLink to="/about">前往 About</NuxtLink>
+          </BaseButton>
+          <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
+            <NuxtLink to="/contact">前往 Contact</NuxtLink>
+          </BaseButton>
+          <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
+            <NuxtLink to="/counter">前往 Counter</NuxtLink>
+          </BaseButton>
+          <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
+            <NuxtLink to="/ofetch/fetch">前往 fetch</NuxtLink>
+          </BaseButton>
+          <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
+            <NuxtLink to="/ofetch/useAsyncData">前往 useAsyncData</NuxtLink>
+          </BaseButton>
+          <BaseButton tag="a" class="mb-3 mb-sm-0" type="info" icon="fa fa-code">
+            <NuxtLink to="/ofetch/useFetch">前往 useFetch</NuxtLink>
+          </BaseButton>
         </div>
       </div>
       <template #fallback>
---
+++ pages/ofetch/useAsyncData.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my-24 flex flex-col items-center">
+    <span class="text-xl text-gray-400">[useAsyncData]</span>
+    <span class="my-2 text-2xl text-gray-600">回傳資料:</span>
+    <p class="my-4 text-xl font-semibold text-blue-500">{{ data }}</p>
+    <p class="my-4 text-xl font-semibold text-blue-500">{{ status }}</p>
+    <p class="my-4 text-xl font-semibold text-blue-500">{{ error }}</p>
+    <button
+      class="mt-6 rounded-md bg-blue-500 px-6 py-3 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+      @click="refresh"
+    >
+      重新獲取資料
+    </button>
+    <button
+      class="mt-6 rounded-md bg-blue-500 px-6 py-3 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+      @click="clear"
+    >
+      清除資料
+    </button>
+  </div>
+</template>
+
+<script setup>
+const { data, status, error, refresh, clear } = await useAsyncData('about', () => $fetch('/api/about'))
+</script>
